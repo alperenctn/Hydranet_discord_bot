540083e47f56c66696b86d7073d90f2e1e93fd6a
Alperen007 <43422692+Alper007@users.noreply.github.com>
2022-11-23 19:15:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -88,12 +88,54 @@ if (msg.member.roles.cache.some(r=>["Admin", "Team", "Moderator"].includes(r.nam
 			}
 		})
 	}
+	}else{
+		if(msg.content.startsWith("!list")){
+			const questions = await Question.find()
+			const q =[]
+			var y = 1;
+			questions.forEach((e)=>{
+				q.push(y+") "+e.keyWord+"---"+ e.question +"---"+ e.answer)
+			y++;
+			})
+			if(questions.length == 0){
+				msg.lineReplyNoMention("list is empty")
+			}else{
+				msg.lineReplyNoMention(q)
+			}
+		}
+		else if(msg.content.startsWith("!questions")){
+			const questionsdb = await Question.find()
+			const questions = []
+			var z = 1;
+			questionsdb.forEach((e)=>{
+			questions.push(z+") "+e.keyWord+"---"+ e.question)
+			z++;
+			})
+			if(questionsdb.length == 0){
+				msg.lineReplyNoMention("list is empty")
+			}else{
+				msg.lineReplyNoMention(questions)
+			}
+		}else if(msg.content.startsWith("!help")){
+			msg.lineReplyNoMention("!add - !delete - !alldelete - !questions - !list - !help");
+		}else{
+			Question.findOne({keyWord:msg.content}, (err,data)=>{
+				if(data){
+					msg.channel.send(data.answer)
+					msg.delete({timeout:5000})
+				}
+			})
+		}
+
 	}
 	}
 });
+
 client.on('message', async (msg) => {
-	if(msg.content == "Wen moon" || msg.content == "wen moon" || msg.content == "when moon"|| msg.content == "When moon" || msg.content == "moon"|| msg.content == "Moon" ){
+	if(msg.content == "Wen moon" || msg.content == "wen moon" || msg.content == "when moon" ||
+	 msg.content == "When moon" || msg.content == "moon"|| msg.content == "Moon" ){
 		msg.lineReplyNoMention("soon")
 	}
 });
 client.login(process.env.token);
+
