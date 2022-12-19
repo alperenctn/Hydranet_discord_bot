a946be0081bffcf94bb16779f9d1e57ff9ab54b1
Alper007 <alperen.1997.cetin@gmail.com>
2022-12-19 14:32:44 +0300

    5

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ require('discord-reply');
 const client = new Client();
 const mongoose = require('mongoose');
 const nodemon = require('nodemon')
+import fetch from 'node-fetch';
+const comma = (x) => {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 
 main().catch(err => console.log(err));
 
@@ -25,8 +29,8 @@ client.once('ready', () => {
 });
 
 client.on('message', async (msg) => {
-
-	if (msg.member.roles.cache.some(r=>["Admin", "Team", "Moderator"].includes(r.name)) ){
+	if(msg.member.roles){
+if (msg.member.roles.cache.some(r=>["Admin", "Team", "Moderator"].includes(r.name))){
 	const firstSpace = msg.content.indexOf(" ")
 	const firstHyphen = msg.content.indexOf("-")
 	const secondHyphen = msg.content.indexOf("-",firstHyphen+1)
@@ -76,7 +80,9 @@ client.on('message', async (msg) => {
 		const keyWordtext = msg.content.slice(firstSpace+1)
 		await Question.find({keyWord:keyWordtext}).deleteOne()
 	}else if(msg.content.startsWith("!alldelete")){
-		Question.deleteMany();
+		await Question.deleteMany();
+	}else if(msg.content.startsWith("!help")){
+		msg.lineReplyNoMention("!add - !delete - !alldelete - !questions - !list - !help");
 	}else{
 		Question.findOne({keyWord:msg.content}, (err,data)=>{
 			if(data){
@@ -85,6 +91,98 @@ client.on('message', async (msg) => {
 			}
 		})
 	}
+	}else if(msg.channel == "hdxinfo-bot"){
+		if(msg.content.startsWith("!list")){
+			const questions = await Question.find()
+			const q =[]
+			var y = 1;
+			questions.forEach((e)=>{
+				q.push(y+") "+e.keyWord+"---"+ e.question +"---"+ e.answer)
+			y++;
+			})
+			if(questions.length == 0){
+				msg.lineReplyNoMention("list is empty")
+			}else{
+				msg.lineReplyNoMention(q)
+			}
+		}
+		else if(msg.content.startsWith("!questions")){
+			const questionsdb = await Question.find()
+			const questions = []
+			var z = 1;
+			questionsdb.forEach((e)=>{
+			questions.push(z+") "+e.keyWord+"---"+ e.question)
+			z++;
+			})
+			if(questionsdb.length == 0){
+				msg.lineReplyNoMention("list is empty")
+			}else{
+				msg.lineReplyNoMention(questions)
+			}
+		}else if(msg.content.startsWith("!help")){
+			msg.lineReplyNoMention("!add - !delete - !alldelete - !questions - !list - !help");
+		}else{
+			Question.findOne({keyWord:msg.content}, (err,data)=>{
+				if(data){
+					msg.channel.send(data.answer)
+					msg.delete({timeout:5000})
+				}
+			})
+		}
+	}
 	}
 });
+
+client.on('message', async (msg) => {
+	if(msg.content == "Wen moon" || msg.content == "wen moon" || msg.content == "when moon" ||
+	 msg.content == "When moon" || msg.content == "moon"|| msg.content == "Moon" ){
+		msg.lineReplyNoMention("soon")
+	}
+});
+
+
+const {BigNumber, ethers } = require("ethers");
+const {ADDRESS, SHDXADDRESS  } = require("./info/address.js");
+const { HDX_ABI, SHDX_ABI} = require("./info/abi.js");
+
+const provider = new ethers.providers.AlchemyProvider( "arbitrum" , process.env.api );
+const _HdxToken = new ethers.Contract(ADDRESS,HDX_ABI,provider);
+const _ShdxToken = new ethers.Contract(SHDXADDRESS, SHDX_ABI,provider);
+
+	fetch('https://api.coingecko.com/api/v3/coins/hydranet/tickers')
+    .then( a => a.json())
+    .then( b => {setPrice(b.tickers[0].last.toString().substr(0,6))})
+
+// StakedHdx= BigNumber.from(StakedHdx).toString().slice(0,-9);
+// setD1(BigNumber.from(shdxAmount).toString().slice(0,-9));
+
+// fetch('https://api.coingecko.com/api/v3/coins/hydranet/tickers')
+// .then( a => a.json())
+// .then( b => {setPrice(b.tickers[0].last.toString().substr(0,6))})
+
+
+client.on('message', async (msg) => {
+	if(msg.content == "!hdx" || msg.content == "!Hdx" || msg.content == "!HDX"  ){
+		var x = await fetch('https://api.coingecko.com/api/v3/coins/hydranet/tickers');
+		var y = await x.json();
+		var z = await y.tickers[0].last.toString().substr(0,6);
+
+		var TotalHdx = await _HdxToken.totalSupply();
+		var StakedHdx = await _HdxToken.balanceOf("0xd20cdf95a08acdf8aa360232caeda6e59a06951d")
+		msg.lineReplyNoMention(
+		"*  HDX SUPPLY  =  "+ comma(BigNumber.from(TotalHdx).toString().slice(0,-9))+'\n'+
+		"*  STAKED HDX  =  "+ comma(BigNumber.from(StakedHdx).toString().slice(0,-9))+'\n'+
+		"*  STAKE RATIO  =  "+ (BigNumber.from(StakedHdx).toString().slice(0,-9)/
+		BigNumber.from(TotalHdx).toString().slice(0,-9)*100).toString().slice(0,-12)+"%"+'\n'+ 
+		"*  INFLATION    =  7.5%"+'\n'+ 
+		"*  CURRENT APY  = " + " 9.4%" + 
+		"*  HDX PRICE  = " + z 
+		)
+		
+	}
+	
+	
+
+});
+
 client.login(process.env.token);
